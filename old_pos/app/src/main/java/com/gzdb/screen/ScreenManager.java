9334b2343a62be26bf5702978557ad3a46d8fe83
EvenLing <979612614@qq.com>
2019-03-08 13:49:41 +0800

    副屏

--- old_pos/app/src/main/java/com/gzdb/screen/ScreenManager.java
+++ old_pos/app/src/main/java/com/gzdb/screen/ScreenManager.java
@@ -1,6 +1,7 @@
 package com.gzdb.screen;
 
 import android.content.Context;
+import android.graphics.Rect;
 import android.hardware.display.DisplayManager;
 import android.util.Log;
 import android.view.Display;
@@ -15,6 +16,7 @@ public class ScreenManager {
     private final String TAG = ScreenManager.class.getSimpleName();
     public static ScreenManager manager = null;
     private Display[] displays = null;//屏幕数组
+    boolean isMinScreen;//小屏
 
     private ScreenManager() {
     }
@@ -34,6 +36,18 @@ public class ScreenManager {
         DisplayManager mDisplayManager = (DisplayManager) context.getSystemService(Context.DISPLAY_SERVICE);
         displays = mDisplayManager.getDisplays();
         Log.d(TAG, "init: ----------->" + displays.length);
+        if (displays.length > 1) {
+            Rect outSize0 = new Rect();
+            displays[0].getRectSize(outSize0);
+
+            Rect outSize1 = new Rect();
+            displays[1].getRectSize(outSize1);
+
+            if (outSize0.right - outSize1.right > 100) {
+                //是小屏
+                isMinScreen = true;
+            }
+        }
     }
 
     public Display[] getDisplays() {
